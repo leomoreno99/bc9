1fc20c381feee448482f9768fa33c6f5e2acbbad
Gino Gutierrez <ginogutierrez95@gmail.com>
2022-07-19 18:39:19 -0300

    Soluciones de katas - multiplos y duplicados

---
+++ src/main/java/ggutierrez/Solution.java
@@ -0,0 +1,12 @@
+package ggutierrez;
+
+public class Solution {
+    public int solution(int number) {
+        int sumador=0;
+        for (int i=0; i<number; i++){
+            if(i%3==0 || i%5==0)
+                sumador+=i;
+        }
+        return sumador;
+    }
+}
