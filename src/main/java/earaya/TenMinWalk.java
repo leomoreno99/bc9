52b679ef6b5521bba8bdb1a80e432c85dfa00798
faheloDev <fahelo.dev@gmail.com>
2022-07-20 09:48:41 -0400

    20 07 :: Edu - resolucion TenMinWalkTest.java

--- src/main/java/earaya/TenMinWalk.java
+++ src/main/java/earaya/TenMinWalk.java
@@ -2,6 +2,33 @@ package earaya;
 
 public class TenMinWalk {
     public static boolean isValid(char[] chars) {
-        return true;
+
+        //clausula
+        if (!(chars.length == 10)) {
+            return false;
+        }
+
+        int nsPosition = 0;
+        int ewPosition = 0;
+
+        for (int i = 0; i < 10; i++) {
+            char caminar = chars[i];
+            switch (caminar) {
+                case 'n':
+                    nsPosition++;
+                    break;
+                case 's':
+                    nsPosition--;
+                    break;
+                case 'e':
+                    ewPosition++;
+                    break;
+                case 'w':
+                    ewPosition--;
+                    break;
+            }
+        }
+        return (nsPosition == 0 && ewPosition == 0); // boolean
+
     }
 }
