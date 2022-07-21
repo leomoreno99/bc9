b7e1a37ef2149087a8375516f2b0d1d5788535f3
cristian.millan <millansoliscristian@gmail.com>
2022-07-20 20:19:01 -0400

    Subiendo kata de tenminwalk

---
+++ src/test/java/cmillan/TenMinWalkTest.java
@@ -0,0 +1,26 @@
+package cmillan;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+Vives en una ciudad,
+donde todas las carreteras están trazadas en una cuadrícula perfecta.
+Llegó diez minutos antes de una cita, por lo que decidió aprovechar la oportunidad para dar un paseo.
+La ciudad ofrece a sus ciudadanos una aplicación de generación de caminatas en sus teléfonos: cada vez que presiona el botón,
+le envía una serie de cadenas de una letra que representan direcciones para caminar (por ejemplo, ['n', 's', 'e', 'o']).
+Siempre caminas solo una cuadra por cada letra (dirección)
+y sabes que te lleva un minuto atravesar una cuadra de la ciudad, así que crea una función que se vuelva verdadera si la caminata
+que te da la aplicación te llevará exactamente diez minutos
+¡No quiero llegar temprano o tarde!) y, por supuesto, lo devolverá a su punto de partida. Devolver falso de lo contrario.
+ */
+public class TenMinWalkTest {
+    @Test
+    public void Test() {
+        assertEquals(true, TenMinWalk.isValid(new char[] {'n','s','n','s','n','s','n','s','n','s'}));
+        assertEquals(false, TenMinWalk.isValid(new char[] {'w','e','w','e','w','e','w','e','w','e','w','e'}));
+        assertEquals(false, TenMinWalk.isValid(new char[] {'w'}));
+        assertEquals( false, TenMinWalk.isValid(new char[] {'n','n','n','s','n','s','n','s','n','s'}));
+    }
+}
