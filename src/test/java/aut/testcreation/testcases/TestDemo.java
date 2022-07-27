46bba4b3736f6d237ef5360a037994fea25384f2
Leonel <leonel_moreno99@hotmail.com>
2022-07-27 11:57:21 -0300

    Acciones del browser parte 2

--- src/test/java/aut/testcreation/testcases/TestDemo.java
+++ src/test/java/aut/testcreation/testcases/TestDemo.java
@@ -5,7 +5,7 @@ import aut.engine.selenium.SeleniumTestBase;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 
 import static aut.engine.utils.Constants.BASE_URL_AUT;
 
@@ -19,7 +19,9 @@ public class TestDemo extends SeleniumTestBase {
         driver = DriverFactory.getDriver();
 
         //Acciones del Browser
-        driver.get(BASE_URL_AUT);
+
+        //Navegacion basica del driver/browser
+        /*driver.get(BASE_URL_AUT);
         obtenerDatosEImprimir();
         Thread.sleep(3000);
 
@@ -34,9 +36,53 @@ public class TestDemo extends SeleniumTestBase {
         driver.navigate().forward();
         obtenerDatosEImprimir();
 
-        driver.navigate().refresh(); //F5
+        driver.navigate().refresh(); //F5*/
 
 
+        // Ventanas y tabs. Como movernos
+        /*String ventanaUno = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.WINDOW);
+        String ventanaDos = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        String pestania = driver.getWindowHandle();
+        driver.get(BASE_URL_AUT);
+
+        driver.switchTo().window(ventanaUno);
+        driver.get(BASE_URL_AUT);
+
+        driver.switchTo().window(ventanaDos);
+        driver.get(BASE_URL_AUT);*/
+
+
+        //Dimensionamiento del browser
+        //int alto = driver.manage().window().getSize().getHeight();
+        //int ancho = driver.manage().window().getSize().getWidth();
+
+        /*Dimension dimensionVentana = driver.manage().window().getSize();
+        int alto = dimensionVentana.getHeight();
+        int ancho = dimensionVentana.getWidth();
+
+        driver.manage().window().setSize(new Dimension(800, 600));
+
+
+        //Posisionamiento del browser
+        Point posisionDriver = driver.manage().window().getPosition();
+        int posisionX = posisionDriver.getX();
+        int posisionY = posisionDriver.getY();
+
+        driver.manage().window().setPosition(new Point(10, 10)); // tambien esta minimize, maximize y fullscreen*/
+
+
+        //Envolver en capa de JavaScript
+        driver.get(BASE_URL_AUT);
+
+        WebElement btnSuerte = driver.findElement(By.name("btnI"));
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("console.log('Hola mundo')");
+        js.executeScript("arguments[0].click()", btnSuerte);
 
         Assertions.assertTrue(true);
     }
