4f449a6f6bd7fd83d62dab4938706acc259b933c
Leonel <leonel_moreno99@hotmail.com>
2022-08-18 19:08:02 -0300

    atc01 vuelos

--- src/test/java/desafio/grupo3/testcases/vuelos/atc01.java
+++ src/test/java/desafio/grupo3/testcases/vuelos/atc01.java
@@ -1,4 +1,30 @@
 package desafio.grupo3.testcases.vuelos;
 
-public class atc01 {
+import desafio.grupo3.pages.HomePageRumbo;
+import desafio.grupo3.pages.vuelos.BusquedaPageVuelos;
+import desafio.grupo3.pages.vuelos.HomePageVuelos;
+import framework.engine.selenium.DriverFactory;
+import framework.engine.selenium.SeleniumTestBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class atc01 extends SeleniumTestBase {
+    HomePageRumbo homePageRumbo;
+    HomePageVuelos homePageVuelos;
+
+    @Test
+    void redireccionIndex() throws InterruptedException {
+        homePageRumbo = new HomePageRumbo(DriverFactory.getDriver());
+        homePageVuelos = new HomePageVuelos(DriverFactory.getDriver());
+        homePageRumbo.navegarAlHome();
+        homePageRumbo.aceptarCookies();
+        homePageRumbo.navegarAPaginaVuelos();
+        homePageVuelos.llenarCamposIdaYVuelta("Madrid", "JFK", 9,3,9,6,2,0,0,"Turista Premium");
+        Thread.sleep(5000);
+        homePageVuelos.buscarVuelos();
+        Thread.sleep(5000);
+        String title = homePageVuelos.getUrlTitle();
+        Assertions.assertEquals("Vuelos baratos | Buscador de vuelos | Rumbo", title);
+    }
 }
